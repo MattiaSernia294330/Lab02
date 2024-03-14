1d3400a479ef3ec1630b5a1a7883853e8859df6f
MattiaSernia294330 <mattiasernia.ms@gmail.com>
2024-03-14 15:39:38 +0100

    problema esercizio 3

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 import translator as tr
 
 t = tr.Translator()
+t.loadDictionary("dictionary.txt")
+scelta=t.printMenu()
+while scelta!=5:
+    if scelta==1:
+        stringa=input("okay, quale parola devo aggiungere?\n")
+        lista=stringa.split(" ")
+        parte_due=''
+        for i in range(1, len(lista)):
+            parte_due+=f" {lista[i]}"
+        tupla=[lista[0], parte_due.lstrip(" ")]
+        t.handleAdd(tupla)
+    elif scelta==2:
+        stringa=input("okay, che parola devo tradurre?\n")
+        print(f'{t.handleTranslate(stringa)}')
+    elif scelta==3:
+        risposta=t.handleWildCard(input("okay, che parola devo tradurre?\n"))
+        if risposta=="":
+            risposta="sei un coglione, non trovo nessuna parola"
+        print(risposta)
+
+    else:
+        print("scegli una delle 5 opzioni")
+    scelta=t.printMenu()
 
 
-while(True):
 
-    t.printMenu()
-
-    t.loadDictionary("filename.txt")
-
-    txtIn = input()
-
-    # Add input control here!
-
-    if int(txtIn) == 1:
-        print()
-        txtIn = input()
-        pass
-    if int(txtIn) == 2:
-        pass
-    if int(txtIn) == 3:
-        pass
-    if int(txtIn) == 4:
-        break
--- translator.py
+++ translator.py
@@ -1,27 +1,65 @@
+import os.path
+import string
+
+
 class Translator:
 
     def __init__(self):
-        pass
+        self.dizionario={}
 
     def printMenu(self):
-        # 1. Aggiungi nuova parola
-        # 2. Cerca una traduzione
-        # 3. Cerca con wildcard
-        # 4. Exit
-        pass
+        numero = int(input("scegli un opzione: \n1. Aggiungi nuova parola\n2. Cerca una traduzione\n3. Cerca con wildcard\n4. Stampa tutto il dizionario\n5. Exit\n"))
+        return numero
 
     def loadDictionary(self, dict):
-        # dict is a string with the filename of the dictionary
-        pass
+        if os.path.exists(dict)!=True:
+            raise ValueError("Errorazzo de Dio")
+        with open(dict, 'r') as file:
+            riga = file.readline()
+            while riga != '':
+                riga = riga.strip("\n")
+                lista = riga.split(" ")
+                if lista[0] not in self.dizionario:
+                    self.dizionario[lista[0]] = lista[1]
+                riga = file.readline()
+        file.close()
 
     def handleAdd(self, entry):
-        # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
-        pass
+        verifica=entry[1].split(" ")
+        abba=""
+        for i in range(0,len(verifica)):
+            abba+=verifica[i]
+        if entry[0].isalpha()==False or abba.isalpha()==False:
+            raise ValueError('Errorazzo de Dio')
+        if entry[0].lower() not in self.dizionario:
+            self.dizionario[entry[0].lower()] = entry[1].lower()
+        with open('dictionary.txt', 'w') as file:
+            for i in self.dizionario:
+                file.write(f'{i} {self.dizionario[i]}\n')
+        file.close()
 
     def handleTranslate(self, query):
-        # query is a string <parola_aliena>
-        pass
+        if query.isalpha()!= True:
+            raise ValueError('Errorazzo de Dio')
+        if query.lower() not in self.dizionario:
+            return "parola non trovata"
+        return self.dizionario[query.lower()]
 
     def handleWildCard(self,query):
-        # query is a string with a ? --> <par?la_aliena>
-        pass
+        query=f"D{query.lower()}D"
+        lista=query.split("?")
+        listazza=[]
+        listazza_parole=[]
+        if lista[0].isalpha()== False or lista[1].isalpha()==False:
+            raise ValueError('Errorazzo de Dio')
+        for i in string.ascii_lowercase:
+            parola=f"{lista[0]}{i}{lista[1]}"
+            parola =parola.rstrip("D").lstrip("D")
+
+            if self.handleTranslate(parola)!="parola non trovata":
+                listazza_parole.append(parola)
+                listazza.append(self.handleTranslate(parola))
+        final=""
+        for i in range(0,len(listazza_parole)):
+            final+=f"{listazza_parole[i]}: {listazza[i]}\n"
+        return final.rstrip("\n")
